ecfde86fc0003f528d5335aa10da6709e72e0428
John Doe <johndoe@example.com>
2023-01-11 15:15:36 -0500

    Third Commit

--- Guessing_Game_Project/Program.cs
+++ Guessing_Game_Project/Program.cs
@@ -1,4 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Omar & Michael");
 
+#region Hide Number
 int myGuess = 8;
+#endregion
+
+Console.WriteLine("What is the number you guess, guess a number between 1-10?");
+int guessNumber = Convert.ToInt32(Console.ReadLine());
+if (guessNumber == myGuess)
+{
+    Console.WriteLine("That is Right");
+}
+else if (guessNumber != myGuess)
+{
+    Console.WriteLine("You chose poorly");
+}
+if (guessNumber > 10 || guessNumber < 1)
+{
+    Console.WriteLine("Please pick numbers between 1-10");
+}
+
