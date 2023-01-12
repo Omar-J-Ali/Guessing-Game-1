fb8e4b9121e3e9c6b901c8638a23cd6bb6e35c35
Omar Ali <kazejin_91423@yahoo.com>
2023-01-12 09:19:44 -0600

    Adding while loop

--- Guessing_Game_Project/Program.cs
+++ Guessing_Game_Project/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Metrics;
+
 Console.WriteLine("Omar & Michael");
 
 #region Hide Number
@@ -7,17 +9,23 @@ int myGuess = 8;
 
 Console.WriteLine("What is the number you guess, guess a number between 1-10?");
 int guessNumber = Convert.ToInt32(Console.ReadLine());
-if (guessNumber == myGuess)
+while (true)
 {
-    Console.WriteLine("That is Right");
+    if (guessNumber == myGuess)
+    {
+        Console.WriteLine("That is Right");
+    }
+    else if (guessNumber != myGuess)
+    {
+        Console.WriteLine("You chose poorly");
+    }
+    if (guessNumber > 10 || guessNumber < 1)
+    {
+        Console.WriteLine("Please pick numbers between 1-10");
+    }
+    if (guessNumber != myGuess)
+    {
+        Console.WriteLine("You chose poorly");
+    }
 }
-else if (guessNumber != myGuess)
-{
-    Console.WriteLine("You chose poorly");
-}
-if (guessNumber > 10 || guessNumber < 1)
-{
-    Console.WriteLine("Please pick numbers between 1-10");
-}
-Console.WriteLine("Made minor change to check username");
-
+    
